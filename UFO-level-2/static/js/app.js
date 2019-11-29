44a157a2efa82b1e93e80f1814740d4f8849b546
saba tassawar <sabatassawar@gmail.com>
2019-11-28 23:04:27 -0500

    UFO 2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,39 +1,88 @@
 // from data.js
 var tableData = data;
+var tbody = document.querySelector("tbody");
+var date_time = document.querySelector("#datetime");
+var city = document.querySelector("#city");
+var state = document.querySelector("#state");
+var country = document.querySelector("#country");
+var shape = document.querySelector("#shape");
 
-// YOUR CODE HERE!
-var tbody = d3.select("tbody")
-tableData.forEach(function(data_js){                    //Loop through data file.
-    var row = tbody.append("tr");                       //Appending rows in table. 
-    
-    Object.entries(data_js).forEach(function([key, value]){
-        var cell = row.append("td").text(value);        //Appending data in each row. 
-    });
-});
+var button_filter = document.querySelector("#filter-btn");     //Defining/selecting button. 
+var button_clear= document.querySelector("#clear-btn");
 
-var button = d3.select("#filter-btn");                  //Defining/selecting button. 
-var button2= d3.select("#clear-btn");                   //Defining/Selecting button 2(clear button)
+button_filter.addEventListener("click", Filter_Click);
 
-button.on("click", function(){                          //Adding button functionality. 
-    
-    var input_element = d3.select(".form-control");      //Selecting the input element upon click. 
-    var input_date = input_element.property("value");     //Getting input value entered. 
+function Table(){
+    tbody.innerHTML = "";
+    for(var i = 0; i< tableData.length; i++){
+        var address = tableData[i];
+        console.log(address)
+        var fields = Object.keys(address);
+
+        var row = tbody.insertRow(i);
+        for (var x= 0;x<fields.length;x++){
+            var field = fields[x];
+            var cell = row.insertCell(x);
+            cell.innerHTML = address[field];
+
+
+        }
+    }
+}
+function Filter_Click() {
+    var table_date = date_time.value;
+    var filter_state = state.value.trim().toLowerCase();
+    var filter_city = city.value.trim().toLowerCase();
+    var filter_country = country.value.trim().toLowerCase();
+    var filter_shape = shape.value.trim().toLowerCase();
   
-    var filtered_Data = tableData.filter(data_js => data_js.datetime === input_date);
+    if (table_date != ""){
+      tableData = data.filter(function(address) {
+        var address_Date = address.datetime; 
+      return address_Date === table_date;
+      });
+    }
+    else {tableData};
+    
+    if(filter_state != ""){
+      tableData = tableData.filter(function(address){
+        var address_State = address.state;
+        return address_State === filter_state;
+      });
+    }
+    else{tableData};
+  
+    if(filter_city != ""){
+      tableData = tableData.filter(function(address){
+        var address_City = address.city;
+        return address_City === filter_city;
+      });
+    }
+    else{ableData};
+  
+    if(filter_country != ""){
+        tableData = tableData.filter(function(address){
+        var address_Country = address.country;
+        return address_Country === filter_country;
+      });
+    }
+    else{tableData};
+  
+    if(filter_shape != "") {
+        tableData = tableData.filter(function(address){
+        var address_Shape = address.shape;
+        return address_Shape === filter_shape;
+      });
+    }
+    else{tableData};
+  
+  Table();
+  }
 
-    var tbody = d3.select("tbody")
-    tbody.html("");                                        //Clear the result after every search
+  // Render the table for the first time on page load
+  Table();
 
-    filtered_Data.forEach(function(data_js){
-        var row = tbody.append("tr");
-       Object.entries(data_js).forEach(function([key, value]){
-       var cell = row.append("td").text(value);
-    })
-  })
-});
-
-button2.on("click", function(){                          //Clear button clears out the table everytime we press it. 
-   
-    tbody.html("");
+  button_clear.addEventListener("click", function(){            //Clear button clears out the table everytime we press it. 
+    tbody.innerHTML = "";
     
 })
